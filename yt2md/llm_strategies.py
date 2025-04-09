56d4e687823c59cf8fadf13756917b7afd97e9fb
GraniLuk <granica.lukasz@gmail.com>
2025-04-09 17:55:23 +0200

    Remove print full prompt, is no longer needed.

--- yt2md/llm_strategies.py
+++ yt2md/llm_strategies.py
@@ -340,7 +340,6 @@ class OllamaStrategy(LLMStrategy):
 
             # Create full prompt
             full_prompt = f"{context_prompt}{template}\n\n{chunk}"
-            print(f"Full prompt: {full_prompt}")
 
             data = {"model": model_name, "prompt": full_prompt, "stream": False}
 
