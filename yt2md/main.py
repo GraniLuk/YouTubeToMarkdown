c25cc0cf4d3917a3c5cd354c282ef486262af370
GraniLuk <granica.lukasz@gmail.com>
2025-05-04 21:10:45 +0200

    Add platform check for beep sound in run_main function; ensure compatibility with non-Windows systems

--- yt2md/main.py
+++ yt2md/main.py
@@ -125,7 +125,8 @@ def run_main(args):
         seconds = execution_time % 60
         logger.info(f"All videos processed in {minutes} min {seconds:.2f} sec")
 
-        winsound.Beep(1000, 500)
+        if os.name == "nt":  # Check if the platform is Windows
+            winsound.Beep(1000, 500)
     except Exception as e:
         logger.error(f"Error in main process: {str(e)}", exc_info=True)
 
